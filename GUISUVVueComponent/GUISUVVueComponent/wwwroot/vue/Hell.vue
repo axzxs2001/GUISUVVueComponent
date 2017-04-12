4be2634f69124f231645f893d3970f166f196b28
桂素伟 <axzxs2001@163.com>
2017-04-12 17:27:34 +0800

    基于jquery的表单验证

---
+++ GUISUVVueComponent/GUISUVVueComponent/wwwroot/vue/Hell.vue
@@ -0,0 +1,18 @@
+﻿<template>
+    <p>{{greeting}} wold!</p>
+</template>
+<script>
+    module.exports={
+    data:function(){
+    return{
+    greeting:'hello'
+    }
+    }
+    }
+</script>
+<style>
+    p{
+    font-size:2em;
+    text-align:center;
+    }
+</style>
