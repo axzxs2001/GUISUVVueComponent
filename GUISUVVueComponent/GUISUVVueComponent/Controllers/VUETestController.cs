6ec31eec45b9fd8e9f63cb6c993c5c5d5c88cfcb
桂素伟 <axzxs2001@163.com>
2017-06-07 14:38:31 +0800

    纯键盘录入。

---
+++ GUISUVVueComponent/GUISUVVueComponent/Controllers/VUETestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GUISUVVueComponent.Controllers
+{
+    public class VUETestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("addperson")]
+        public IActionResult AddPerson(string name,string sex,int age,string mz,double height)
+        {
+            return new JsonResult(new { name = name, sex = sex, age = age,mz=mz, height = height });
+        }
+    }
+}
